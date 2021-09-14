7bf95b4f440e7a96ccf75b397b527295ce36d930
Eston Kagwima <ekagwima745@gmail.com>
2021-09-14 19:04:33 +0300

    setting up email.py and templates

---
+++ analytics/email.py
@@ -0,0 +1,15 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_activation_email(name,receiver):
+    # Creating message subject and sender
+    subject = 'Welcome to Our Nakala analaytics Application'
+    sender = 'EKmuraya@gmail.com'
+
+    #passing in the context vairables
+    text_content = render_to_string('email/activation.txt',{"name": name})
+    html_content = render_to_string('email/activation.html',{"name": name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
